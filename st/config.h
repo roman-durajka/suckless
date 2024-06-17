2db0aaf1c398a79a03dd0124db1e56e56e4fc0fc
Roman Durajka <romo.durajka@gmail.com>
2024-06-17 22:12:19 +0200

    changed fonts

--- st/config.h
+++ st/config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
+//static char *font = "Iosevka:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Iosevka:pixelsize=23:antialias=true";
 static int borderpx = 2;
 
 /*
