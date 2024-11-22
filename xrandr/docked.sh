a86e1c32ec8dfa3c841d1d3ee3442ad3505da72c
roman-durajka <95091748+roman-durajka@users.noreply.github.com>
2024-11-22 10:09:43 +0100

    update xrandr

--- xrandr/docked.sh
+++ xrandr/docked.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xrandr --output DP2-1 --auto --primary --left-of DP2-2
-xrandr --output eDP1 --off
+xrandr --output DP-2-1 --auto --primary --left-of DP-2-2
+xrandr --output eDP-1 --off
